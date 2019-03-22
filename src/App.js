d48a5751b6f9eb97686f5b7f67c5ac5380365939
Bobby <dev@bthompson.org>
2019-03-22 09:21:54 -0700

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import eye from './assets/eye_transBG.png';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark" sticky="top">
+                <Navbar.Brand href="#home">
+                <img alt="" src={eye} width="30" height="30"
+                    className="d-inline-block align-top"
+                    id="brand"
+                />
+                {' blueeyes.dev'}
+                </Navbar.Brand>
+                <Nav fill>
+                    <Nav.Item>
+                        <Nav.Link href="#home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#aboutme">About Me</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="#gallery">Gallery</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Navbar>
+            <Container fluid>
+                <Row></Row>
+                <Row>
+                    <Col xs={2}></Col>
+                    <Col xs={8}>
+                        <Jumbotron fluid>
+                            <p>This is some text inside of the jumbotron</p>
+                        </Jumbotron>
+                    </Col>
+                    <Col xs={2}></Col>
+                </Row>
+            </Container>
+        </div>
+        );
+    }
 }
 
 export default App;
